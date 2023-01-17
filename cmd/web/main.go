eab07ab6e31022742db04d2fd3bb8d6ec3de69a4
Burak Yeşilyurt <burakyesilyurt@duck.com>
2023-01-17 18:16:41 +0300

    initial commit

---
+++ cmd/web/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+const portNumber = "8080"
+const ipAddress = "127.0.0.1"
+
+var url = fmt.Sprintf("%s:%s", ipAddress, portNumber)
+
+func main() {
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/yesilyurtburak/go-web-basics-3
+
+go 1.19
