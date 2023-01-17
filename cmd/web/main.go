36bd45e71c23b6d2762ebc2ee688a656e085ff30
Burak Yeşilyurt <burakyesilyurt@duck.com>
2023-01-17 19:34:49 +0300

    added functions for routing pages and listening http requests

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/yesilyurtburak/go-web-basics-3/pkg/handlers"
 )
 
 const portNumber = "8080"
@@ -10,5 +14,12 @@ const ipAddress = "127.0.0.1"
 var url = fmt.Sprintf("%s:%s", ipAddress, portNumber)
 
 func main() {
+	// routing pages
+	http.HandleFunc("/", handlers.HomeHandler)
+	http.HandleFunc("/about", handlers.AboutHandler)
 
+	// listen to the port for incoming http requests.
+	fmt.Printf("Listening traffic at %s\n", url)
+	err := http.ListenAndServe(url, nil) // 2nd parameter is `nil` since we didn't send any information to the page.
+	log.Fatal(err)
 }
