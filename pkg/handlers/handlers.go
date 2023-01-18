e9f19ffcc6b841be691c46e74b026b33323e896e
Burak Yeşilyurt <burakyesilyurt@duck.com>
2023-01-18 14:34:24 +0300

    added an example cookie

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -29,6 +29,10 @@ func NewHandlers(r *Repository) {
 }
 
 func (m *Repository) HomeHandler(w http.ResponseWriter, r *http.Request) {
+	// set up an example cookie
+	// whenever user goes to the homepage, they'are going to automatically have the userid placed inside of here as if they were entered it.
+	m.App.Session.Put(r.Context(), "userid", "burakyesilyurt")
+
 	render.RenderTemplate(w, "home.page.gotmpl", &models.PageData{})
 }
 
@@ -36,6 +40,11 @@ func (m *Repository) AboutHandler(w http.ResponseWriter, r *http.Request) {
 	strMap := make(map[string]string)
 	strMap["title"] = "About Us"
 	strMap["content"] = "We like to talk about ourselves here"
+
+	// get `userid` from the session.
+	userid := m.App.Session.GetString(r.Context(), "userid")
+	strMap["userid"] = userid // add it to the strMap to send to the page template.
+
 	render.RenderTemplate(w, "about.page.gotmpl", &models.PageData{StrMap: strMap})
 	// created a strMap and send some information to about.page.gotmpl template via models.PageData
 }
